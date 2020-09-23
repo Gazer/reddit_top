364ce8b7ae2cb3e84264c1e16366c58fc907eda0
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2020-09-23 13:55:04 -0300

    add basic layouts for the item list

--- app/src/main/java/com/example/reddittop/MainActivity.kt
+++ app/src/main/java/com/example/reddittop/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.reddittop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.reddittop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        ActivityMainBinding.inflate(layoutInflater, null, false).also {
+            binding = it
+        }.run {
+            setContentView(root)
+        }
+
+        binding.toolbar.title = getString(R.string.app_name)
     }
 }
