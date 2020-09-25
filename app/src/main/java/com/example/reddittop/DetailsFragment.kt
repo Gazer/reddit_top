8f3a5ffcd15f7deab1be8b2b3798bd9987bff553
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2020-09-25 12:46:19 -0300

    save image to gallery and cleanups

--- app/src/main/java/com/example/reddittop/DetailsFragment.kt
+++ app/src/main/java/com/example/reddittop/DetailsFragment.kt
@@ -1,12 +1,17 @@
 package com.example.reddittop
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.reddittop.dao.TopEntry
@@ -15,6 +20,7 @@ import com.example.reddittop.viewModel.RedditViewModel
 import com.example.reddittop.viewModel.RedditViewModelFactory
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.BufferedOutputStream
 import java.util.*
 import javax.inject.Inject
 
@@ -72,7 +78,11 @@ class DetailsFragment : Fragment() {
 
         val image = item.imageUrl
         if (image != null) {
-            Picasso.get().load(image).into(binding.previewImage);
+            Picasso.get().load(image).into(binding.previewImage)
+            binding.previewImage.setOnLongClickListener {
+                saveImage(item.id)
+                true
+            }
             binding.previewImage.visibility = View.VISIBLE
         } else {
             binding.previewImage.visibility = View.GONE
@@ -86,5 +96,40 @@ class DetailsFragment : Fragment() {
 
         binding.scrollable.scrollTo(0, 0)
         binding.container.visibility = View.VISIBLE
+    }
+
+    private fun saveImage(id: String) {
+        // Quick & Dirty save
+        binding.previewImage.setDrawingCacheEnabled(true)
+        val bitmap: Bitmap = binding.previewImage.getDrawingCache()
+
+        addImageToGallery("reddit_$id.jpg", bitmap)
+        binding.previewImage.setDrawingCacheEnabled(false)
+
+        Toast.makeText(requireContext(), "Image saved", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun addImageToGallery(fileName: String, bitmap: Bitmap) {
+        val values = ContentValues()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+        }
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        values.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, fileName)
+        values.put(MediaStore.Images.ImageColumns.TITLE, fileName)
+
+        val uri: Uri? = requireActivity().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )
+        uri?.let {
+            requireActivity().contentResolver.openOutputStream(uri)?.let { stream ->
+                val oStream =
+                    BufferedOutputStream(stream)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, oStream)
+                oStream.close()
+            }
+        }
+
     }
 }
